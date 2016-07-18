ea4d905cf5ac832635763b4cd500c34dfa45fd6e
Neil Martinsen-Burrell <neil@moz.com>
2016-07-17 20:54:38 -0500

    Add very basic sweep and plot

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
 const serial = chrome.serial;
 
+const INITIAL_FREQUENCY = 14150000;
+
 var connectedId = null;
+var frequency = INITIAL_FREQUENCY;
+
+var from_frequency = 4000000;
+var to_frequency = 12000000;
+var step_size = 100000;
+
+var sweep_readings = [];
 
 function writeSerial(str) {
-  console.log('Sending ' + str);
   serial.send(connectedId, convertStringToArrayBuffer(str), function(){});
 }
 // Convert string to ArrayBuffer
-var convertStringToArrayBuffer=function(str) {
+function convertStringToArrayBuffer(str) {
   var buf=new ArrayBuffer(str.length);
   var bufView=new Uint8Array(buf);
   for (var i=0; i<str.length; i++) {
@@ -16,19 +24,10 @@ var convertStringToArrayBuffer=function(str) {
   return buf;
 }
 // Convert ArrayBuffer to string
-function convertArrayBufferToString(buf){
-  console.log('Buffer length');
-  console.log(buf.byteLength);
+function convertArrayBufferToString(buf) {
   var bufView = new Uint8Array(buf);
-  console.log('bufView:');
-  console.log(bufView);
   var encodedString = String.fromCharCode.apply(null, bufView);
-  console.log('encodedString:');
-  console.log(encodedString);
-  var returning = decodeURIComponent(escape(encodedString))
-  console.log('returning:');
-  console.log(returning);
-  return returning;
+  return decodeURIComponent(escape(encodedString));
 }
 
 var stringReceived = '';
@@ -36,7 +35,6 @@ var stringReceived = '';
 function onReceiveCallback(info) {
   if (info.connectionId == connectedId && info.data) {
     var str = convertArrayBufferToString(info.data);
-    console.log('Received string ' + str + ' of length ' + str.length);
     if (str.charAt(str.length-1) === '\n') {
       stringReceived += str.substring(0, str.length-1);
       onLineReceived(stringReceived);
@@ -45,32 +43,116 @@ function onReceiveCallback(info) {
       stringReceived += str;
     }
   }
-};
+}
 serial.onReceive.addListener(onReceiveCallback);
 
-function onLineReceived(line) {
-  console.log('Received line: ' + line);
+function onLineReceived(lines) {
+  console.log('Received lines: ' + lines);
+  $.each(lines.split('\n'), function(index, line) {
+    switch(line[0]) {
+      case "d":
+        // detector reading as 10-bit integer
+        updatePower(line.substring(1));
+        break;
+      case "b":
+        // start of begin
+        if (line == 'begin') {
+          console.log('beginning sweep');
+          doing_sweep = true;
+          sweep_readings = [];
+        } else {
+          console.log('got b without begin');
+        }
+        break;
+      case "r":
+        // reading in sweep
+        var freq_val = line.substring(1).split(':');
+        sweep_readings.push([Number(freq_val[0])/1000000, Number(freq_val[1])/10]);
+        break;
+      case "e":
+        // start of end
+        if (line == 'end') {
+          console.log('ending sweep');
+          doing_sweep = false;
+          console.log("Got " + sweep_readings.length + " readings");
+        } else {
+          console.log('got e without end');
+        }
+        plotReadings(sweep_readings);
+        break;
+      default:
+        // no match
+        console.log('No matching case for line: ' + line);
+        break;
+    }
+  });
+}
+
+function updatePower(line) {
+  // Farhan's analogRead to dBm curve from specan.exe
+  document.getElementById('power').innerHTML = (Number(line) * 0.2 - 92).toFixed(1).toString() + ' dBm';
+}
+
+function plotReadings(readings) {
+  $.plot('#graphics', [{data: readings}],
+        {
+          series: {
+            lines: { show: true },
+            points: { show: true }
+          },
+          xaxis: {
+            ticks: 20
+          },
+          yaxis: {
+            ticks: 10,
+            min: -100,
+            max: 0,
+            tickDecimals: 0
+          },
+          grid: {
+            backgroundColor: { colors: [ "#fff", "#eee" ] },
+            borderWidth: {
+              top: 1,
+              right: 1,
+              bottom: 2,
+              left: 2
+            }
+          }
+         }
+       );
 }
 
 function readDetector() {
   writeSerial('r\n');
 }
 
+function setFrequency(freq) {
+  frequency = freq;
+  writeSerial('f' + freq.toString() + '\n');
+}
+
 function connect(e) {
   console.log('Connect clicked!');
   var path = document.getElementById('serialports').value
-  serial.connect(path, { "bitrate": 115200 }, function(connectionInfo) {
+  serial.connect(path, { "bitrate": 9600 }, function(connectionInfo) {
     console.log('Connected with connection info:');
     console.log(connectionInfo);
     connectedId = connectionInfo.connectionId;
-    writeSerial('i\n');
-    setInterval(readDetector, 10000);
+
+    // Set initial frequency
+    setFrequency(INITIAL_FREQUENCY);
+    setInterval(readDetector, 1000);
   });
   return true;
 }
 
 function sweep(e) {
   console.log('Sweep clicked!');
+  setFrequency(from_frequency);
+  writeSerial('t' + to_frequency.toString() + '\n');
+  writeSerial('s' + step_size + '\n');
+  writeSerial('g\n');
+  return true;
 }
 
 function loadDevices(ports) {
